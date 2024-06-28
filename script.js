422d6e5261f89c020822212f83947489109203ac
Noah Wang <bwang.noah@gmail.com>
2024-06-28 12:54:03 -0700

    changes to AC, C, and numbers with errorchecking

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const OPERATORS = ['/', '-', '+', 'x', '%'];
 const NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const DECIMAL_SYMBOL = '.';
 const PLUS_MINUS = '+/-';
+const ROUND_DECIMAL = 10;
+const UPPER_BOUND_NUMBER_LEN = 16;
+const ERROR_OUT_OF_BOUNDS = "ERROR: Number Out of Bounds...";
 
 
 function add(a, b) {
@@ -64,8 +67,72 @@ function displayResult(result) {
 }
 
 
+function clearAll() {
+    expression_result = 0;
+    number = 0;
+    operator_type = null;
+    displayExpression(0);
+    displayResult(0);
+}
+
+
+function clearChar() {
+    if (number != 0) {
+        clearLastChar = number.slice(0, number.length - 1);
+        displayExpression(clearLastChar);
+        number = clearLastChar;
+    }
+    else if (number === "") {
+        displayExpression(0);
+        number = 0;
+    }
+}
+
+
+function convertToString(value) {
+    return String(value);
+}
+
+
+function convertToInt(value) {
+    intValue = parseFloat(value);
+    if (isNaN(intValue)) {
+        return null;
+    }
+    if (convertToString(intValue).length >= UPPER_BOUND_NUMBER_LEN) {
+        return 0;
+    }
+    return intValue;
+}
+
+
+function formNumber(num) {
+    return convertToInt(convertToString(number) + num);
+}
+
+
+function parseNumber(num) {
+    if (!operator_type) {
+        newNum = number == 0 ? num : formNumber(num);
+        if (newNum === 0 && number !== 0) 
+            displayExpression(ERROR_OUT_OF_BOUNDS);
+        else
+            displayExpression(newNum);
+        number = newNum;
+    }
+}
+
+
 function parseButton(value) {
-    
+    if (value === "AC") {
+        clearAll(); 
+    }
+    else if (value === "C") {
+        clearChar();
+    }
+    else if (NUMBERS.includes(value)) {
+        parseNumber(value);
+    }
 }
 
 
@@ -74,7 +141,7 @@ buttons.forEach(button => {
     button.addEventListener("click", (event) => {
         console.log(expression_result, number, operator_type);
         const value = event.target.value;
-
+        parseButton(value);
     });
 });
 
